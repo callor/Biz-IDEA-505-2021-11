7f55af82c7b9d73633448208df3fbc7f93e115bd
callor <callor@callor.com>
2021-11-02 11:32:47 +0900

    firstProject

---
+++ Spring_00_Hello/src/main/java/com/callor/hello/HomeController.java
@@ -0,0 +1,16 @@
+package com.callor.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @ResponseBody
+    @RequestMapping(value="/",method=RequestMethod.GET)
+    public String home() {
+        return "반갑습니다 나는 IntelliJ IDEA 입니다";
+    }
+}
